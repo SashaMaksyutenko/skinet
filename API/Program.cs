942605ed1d5f5254feac31c807be2ea6a4448754
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2022-11-29 18:59:32 +0200

    section3 completed

--- API/Program.cs
+++ API/Program.cs
@@ -1,18 +1,35 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-builder.Services.AddDbContext<StoreContext>(x=>
+builder.Services.AddDbContext<StoreContext>(x=> 
 x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
 var app = builder.Build();
+var services = builder.Services.BuildServiceProvider();
+var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+try
+{
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context,loggerFactory);
+}
 
+catch(Exception ex)
+{
+    var logger = loggerFactory.CreateLogger<Program>();
+    logger.LogError(ex, "An error occured during migration");
+}
+    
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -27,3 +44,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
