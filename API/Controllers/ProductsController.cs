be8c14386c9dde9094978812ead25f712c2ba2ca
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2022-12-05 16:15:57 +0200

    commit 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using Core.Specifications;
 using API.Dtos;
 using AutoMapper;
 using API.Errors;
+using API.Helpers;
 
 namespace API.Controllers
 {
@@ -26,11 +27,14 @@ namespace API.Controllers
             _productTypeRepo = productTypeRepo;
         }
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts([FromQuery]ProductSpecParams productParams)
         {
-            var spec=new ProductsWithTypesAndBrandsSpecification();
+            var spec=new ProductsWithTypesAndBrandsSpecification(productParams);
+            var countSpec=new ProductWithFiltersForCountSpecification(productParams);
+            var totalItems=await _productsRepo.CountAsync(countSpec);
             var products = await _productsRepo.ListAsync(spec);
-            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
+            var data=_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
+            return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex,productParams.PageSize,totalItems,data));
         }
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -12,14 +12,11 @@ namespace Infrastructure.Data
         public ProductRepository(StoreContext context)
         {
             _context = context;
-
         }
-
         public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
         {
             return await _context.ProductBrands.ToListAsync();
         }
-
         public async Task<Product> GetProductByIdAsync(int id)
         {
             return await _context.Products
@@ -27,17 +24,16 @@ namespace Infrastructure.Data
             .Include(p=>p.ProductBrand)
             .FirstOrDefaultAsync(p=>p.Id==id);
         }
-
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
            // var typeId=1;
            // var products=_context.Products.Where(x=>x.ProductTypeId==typeId).Include(x=>x.ProductType).ToListAsync();
+            //var products=_context.Products.OrderBy();
             return await _context.Products
             .Include(p=>p.ProductType)
             .Include(p=>p.ProductBrand)
             .ToListAsync();
         }
-
         public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
         {
              return await _context.ProductTypes.ToListAsync();
